e1eff457c1b995c5d370a44f7f398939ae954526
katastrof1x <168102591+katastrof1x@users.noreply.github.com>
2024-05-16 11:55:09 +0300

    Add files via upload

---
+++ Tasc 5.2.py
@@ -0,0 +1,29 @@
+задача 1
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл"]
+last_player = list_players[-1]
+reestr = {"Первый участник": list_players[0]}
+print("Имя последнего игрока:", last_player)
+print("Имя первого игрока из словаря reestr:", reestr["Первый участник"])
+задача 2
+list_participants = ["Орлов", "Петров", "Иванов", "Сидоров", "Васильев", "Черепахин"]
+last_participant = list_participants[-1]
+winner = {"Первый лыжник": list_participants[0]}
+print("Имя последнего лыжника:", last_participant)
+print("Имя первого лыжника из словаря winner:", winner["Первый лыжник"])
+задача 3
+list_participants = ["Орлов", "Петров", "Иванов", "Сидоров", "Васильев", "Черепахин"]
+last_participant = list_participants[-1]
+winner = {"Первый лыжник": list_participants[0]}
+print("Имя последнего лыжника:", last_participant)
+print("Имя первого лыжника из словаря winner:", winner["Первый лыжник"])
+задание 4
+sps_books = ["Дубровский", "Горе от ума", "Кавказский пленник", "Хамелеон", "Ревизор", "Гранатовый браслет"]
+last_book = sps_books[-1]
+books_dict = {"Пушкин": sps_books[0]}
+print("Название последней книги:", last_book)
+print("Название первой книги из словаря по ключу 'Пушкин':", books_dict["Пушкин"])
+задание 5
+shopping_list = ["Палатка", "Спальник", "Котелок", "Тренога", "Рюкзак", "Спальник", "Рюкзак", "Термос", "Миска", "Ветровка", "Коврик"]
+unique_items = set(shopping_list)
+num_unique_items = len(unique_items)
+print("Количество уникальных товаров:", num_unique_items)
